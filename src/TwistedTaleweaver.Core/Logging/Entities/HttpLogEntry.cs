76fe83bd17b23df8f406abbcdc950d6d041322dd
Daniel Pindur <pindurdan@gmail.com>
2025-06-26 16:56:09 +0200

    feat: add twitch rest api generated clients

---
+++ src/TwistedTaleweaver.Core/Logging/Entities/HttpLogEntry.cs
@@ -0,0 +1,33 @@
+namespace TwistedTaleweaver.Core.Logging.Entities;
+
+/// <summary>
+/// Represents a single HTTP log entry.
+/// </summary>
+public sealed class HttpLogEntry
+{
+    /// <summary>
+    /// The timestamp when the log entry was created.
+    /// </summary>
+    public DateTimeOffset Timestamp { get; set; }
+
+    /// <summary>
+    /// The request information associated with the log entry.
+    /// </summary>
+    public required RequestInfo Request { get; set; }
+
+    /// <summary>
+    /// The response information associated with the log entry, if available.
+    /// </summary>
+    public ResponseInfo? Response { get; set; }
+
+    /// <summary>
+    /// The exception information associated with the log entry, if an exception occurred.
+    /// </summary>
+    public ExceptionInfo? Exception { get; set; }
+
+    /// <summary>
+    /// The duration in milliseconds that the request took to process.
+    /// This is set if the request was successful or if an exception occurred.
+    /// </summary>
+    public double? DurationMs { get; set; }
+}
---
+++ src/TwistedTaleweaver.Integration.Twitch/OAuth2/Client/ApiResponseEventArgs.cs
@@ -0,0 +1,22 @@
+namespace TwistedTaleweaver.Integration.Twitch.OAuth2.Client
+{
+    /// <summary>
+    /// Useful for tracking server health
+    /// </summary>
+    public class ApiResponseEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The ApiResponse
+        /// </summary>
+        public ApiResponse ApiResponse { get; }
+
+        /// <summary>
+        /// The ApiResponseEventArgs
+        /// </summary>
+        /// <param name="apiResponse"></param>
+        public ApiResponseEventArgs(ApiResponse apiResponse)
+        {
+            ApiResponse = apiResponse;
+        }
+    }
+}
