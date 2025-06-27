28286188fc19d5a6201747e9043d70499bbf47ee
Daniel Pindur <pindurdan@gmail.com>
2025-06-27 09:54:06 +0200

    fix: apply suggestions from code review

--- src/TwistedTaleweaver.Core/Logging/Entities/RequestInfo.cs
+++ src/TwistedTaleweaver.Core/Logging/Entities/RequestInfo.cs
@@ -8,30 +8,30 @@ public sealed class RequestInfo
     /// <summary>
     /// The HTTP method used for the request (e.g., GET, POST).
     /// </summary>
-    public required string Method { get; set; }
+    public required string Method { get; init; }
 
     /// <summary>
     /// The URL of the request.
     /// </summary>
-    public required string Url { get; set; }
+    public required string Url { get; init; }
 
     /// <summary>
     /// The HTTP version used for the request (e.g., HTTP/1.1).
     /// </summary>
-    public required string HttpVersion { get; set; }
+    public required string HttpVersion { get; init; }
 
     /// <summary>
     /// A collection of headers associated with the request.
     /// </summary>
-    public Dictionary<string, string> Headers { get; set; } = new();
+    public Dictionary<string, string> Headers { get; init; } = new();
 
     /// <summary>
     /// The body of the request, if available.
     /// </summary>
-    public string? Body { get; set; }
+    public string? Body { get; init; }
 
     /// <summary>
     /// The length of the content in the request body, if available.
     /// </summary>
-    public int? ContentLength { get; set; }
+    public int? ContentLength { get; init; }
 }
--- src/TwistedTaleweaver.Core/Logging/Entities/ResponseInfo.cs
+++ src/TwistedTaleweaver.Core/Logging/Entities/ResponseInfo.cs
@@ -8,25 +8,25 @@ public sealed class ResponseInfo
     /// <summary>
     /// The HTTP status code of the response (e.g., 200, 404).
     /// </summary>
-    public int StatusCode { get; set; }
+    public int StatusCode { get; init; }
 
     /// <summary>
     /// The reason phrase associated with the status code (e.g., "OK", "Not Found").
     /// </summary>
-    public string? ReasonPhrase { get; set; }
+    public string? ReasonPhrase { get; init; }
 
     /// <summary>
     /// Headers associated with the response.
     /// </summary>
-    public Dictionary<string, string> Headers { get; set; } = new();
+    public Dictionary<string, string> Headers { get; init; } = new();
 
     /// <summary>
     /// The body of the response, if available.
     /// </summary>
-    public string? Body { get; set; }
+    public string? Body { get; init; }
 
     /// <summary>
     /// The length of the content in the response body, if available.
     /// </summary>
-    public int? ContentLength { get; set; }
+    public int? ContentLength { get; init; }
 }
