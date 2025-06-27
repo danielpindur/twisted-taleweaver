303c9d9e4392a3254e48300f46797410793cb102
Daniel Pindur <pindurdan@gmail.com>
2025-06-27 10:22:12 +0200

    chore: resolve bad merge

--- src/TwistedTaleweaver.Database/Program.cs
+++ src/TwistedTaleweaver.Database/Program.cs
@@ -1,4 +1,4 @@
-﻿using FluentMigrator.Runner;
+using FluentMigrator.Runner;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
